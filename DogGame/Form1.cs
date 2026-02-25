db0df46f11d616803a685c19eb8d8428ffac4d30
Onyx <peachii12nn@gmail.com>
2026-02-25 11:34:52 +0700

    Minor Update

--- DogGame/Form1.cs
+++ DogGame/Form1.cs
@@ -12,6 +12,18 @@ namespace DogGame
 {
     public partial class Form1: Form
     {
+        #region player controller
+        bool moveLeft, moveRight, moveUp, moveDown;
+        #endregion
+
+        #region animation system
+        public Image[] frames;
+        public int currentFrame = 0;
+        public int sampleRate = 5;
+        private int frameCounter = 0;
+        public Point position;
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +31,23 @@ namespace DogGame
 
         public void Test()
         {
-            //test test dog หมา หมา
+            //test test dog หมา หมา อย่าพึ่งทำไรนะ
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A) moveLeft = true;
+            if (e.KeyCode == Keys.D) moveRight = true;
+            if (e.KeyCode == Keys.W) moveUp = true;
+            if (e.KeyCode == Keys.S) moveDown = true;
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A) moveLeft = false;
+            if (e.KeyCode == Keys.D) moveRight = false;
+            if (e.KeyCode == Keys.W) moveUp = false;
+            if (e.KeyCode == Keys.S) moveDown = false;
         }
     }
 }
